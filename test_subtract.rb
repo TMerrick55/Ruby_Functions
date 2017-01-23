aea364b4c45e419e8c13a52bda03249caac1fa90
TMerrick55 <tmerrick44@gmail.com>
2017-01-23 10:41:18 -0500

    Addition/Subtraction Ver. 5.0

--- test_subtract.rb
+++ test_subtract.rb
@@ -9,4 +9,8 @@ class TestMMFunctions < Minitest::Test
 	def test_five_minus_two_equals_three
 		assert_equal(3, subtract(5,2))
 	end
+
+	def test_negative_three_minus_negative_four_equals_negative_one
+		assert_equal(1, subtract(-3,-4))
+	end
 end
