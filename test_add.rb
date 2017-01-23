10aa7a470e8fc4a1463ceab6994e68ea88b0463b
TMerrick55 <tmerrick44@gmail.com>
2017-01-23 09:56:46 -0500

    Addition Ver. 1.0

---
+++ test_add.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require_relative 'add.rb'
+
+class TestMMFunctions < Minitest::Test
+	def test_assert_equal_1_equals_1
+		assert_equal(1,1)
+	end
+
+	def test_functions_one_plus_one_equals_two
+		assert_equal(2, add(1,1))
+	end
+end
